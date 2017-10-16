8bb762e61a76c6b1b653d09999ebe42d21c3c030
Vlad <gregurco.vlad@gmail.com>
2017-10-16 10:33:47 +0300

    Init

---
+++ README.md
@@ -0,0 +1,61 @@
+Guzzle WSSE Plugin
+==================
+This plugin integrates [WSSE][1] functionality into Guzzle, a PHP framework for building RESTful web service clients.
+
+
+Requirements
+------------
+ - PHP 7.0 or above
+ - [Guzzle Bundle][2]
+
+ 
+Installation
+------------
+Using [composer][3]:
+
+``` json
+{
+    "require": {
+        "gregurco/guzzle-wsse-plugin": "dev-master"
+    }
+}
+```
+
+
+Usage
+-----
+Load plugin in AppKernel.php:
+``` php
+new EightPoints\Bundle\GuzzleBundle\GuzzleBundle([
+    new Gregurco\Bundle\EightPointsGuzzleWssePlugin\EightPointsGuzzleWssePlugin(),
+])
+```
+
+
+``` php
+<?php 
+
+$wsse = new \Gregurco\Bundle\EightPointsGuzzleWssePlugin\Middleware\WsseAuthMiddleware($username, $password);
+
+# Optional: Set createdAt by a expression (if not, current time will be used automatically)
+# http://php.net/manual/en/datetime.formats.relative.php
+# Useful if there is a small difference of time between client and server
+# DateTime object will be regenerated for every request
+$wsse->setCreatedAtTimeExpression('-10 seconds');
+
+$stack = \GuzzleHttp\HandlerStack::create();
+
+// Add the wsse middleware to the handler stack.
+$stack->push($wsse->attach());
+
+$client   = new \GuzzleHttp\Client(['handler' => $stack]);
+$response = $client->get('http://www.8points.de');
+```
+
+License
+-------
+This middleware is licensed under the MIT License - see the LICENSE file for details
+
+[1]: http://www.xml.com/pub/a/2003/12/17/dive.html
+[2]: https://github.com/8p/GuzzleBundle
+[3]: https://getcomposer.org/
---
+++ src/EightPointsGuzzleWssePlugin.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Gregurco\Bundle\EightPointsGuzzleWssePlugin;
+
+use Gregurco\Bundle\EightPointsGuzzleWssePlugin\DependencyInjection\GuzzleWsseExtension;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\ExpressionLanguage\Expression;
+
+class EightPointsGuzzleWssePlugin extends Bundle
+{
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container) : void
+    {
+        $extension = new GuzzleWsseExtension();
+        $extension->load($configs, $container);
+    }
+
+    /**
+     * @param array $config
+     * @param ContainerBuilder $container
+     * @param string $clientName
+     * @param Definition $handler
+     */
+    public function loadForClient(array $config, ContainerBuilder $container, string $clientName, Definition $handler)
+    {
+        if ($config['username'] && $config['password']) {
+            $username = $config['username'];
+            $password = $config['password'];
+            $createdAtExpression = null;
+
+            if (isset($config['created_at']) && $config['created_at']) {
+                $createdAtExpression = $config['created_at'];
+            }
+
+            $wsse = new Definition('%eight_points_guzzle.middleware.wsse.class%');
+            $wsse->setArguments([$username, $password]);
+
+            if ($createdAtExpression) {
+                $wsse->addMethodCall('setCreatedAtTimeExpression', [$createdAtExpression]);
+            }
+
+            $wsseServiceName = sprintf('eight_points_guzzle.middleware.wsse.%s', $clientName);
+
+            $container->setDefinition($wsseServiceName, $wsse);
+
+            $wsseExpression = new Expression(sprintf('service("%s").attach()', $wsseServiceName));
+
+            $handler->addMethodCall('push', [$wsseExpression]);
+        }
+    }
+
+    /**
+     * @param ArrayNodeDefinition $pluginNode
+     */
+    public function addConfiguration(ArrayNodeDefinition $pluginNode) : void
+    {
+        $pluginNode
+            ->addDefaultsIfNotSet()
+            ->children()
+                ->scalarNode('username')->defaultFalse()->end()
+                ->scalarNode('password')->defaultValue('')->end()
+                ->scalarNode('created_at')->defaultFalse()->end()
+            ->end();
+    }
+
+    /**
+     * @return string
+     */
+    public function getPluginName() : string
+    {
+        return 'wsse';
+    }
+}
