b1633e9e03e50e668d245c36718dbde4979f7de8
Vlad <gregurco.vlad@gmail.com>
2017-10-19 09:42:21 +0300

    add tests

--- tests/GuzzleBundleWssePluginTest.php
+++ tests/GuzzleBundleWssePluginTest.php
@@ -5,6 +5,7 @@ namespace Gregurco\Bundle\GuzzleBundleWssePlugin\Test;
 use EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundlePlugin;
 use Gregurco\Bundle\GuzzleBundleWssePlugin\GuzzleBundleWssePlugin;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class GuzzleBundleWssePluginTest extends TestCase
@@ -15,5 +16,27 @@ class GuzzleBundleWssePluginTest extends TestCase
 
         $this->assertInstanceOf(EightPointsGuzzleBundlePlugin::class, $plugin);
         $this->assertInstanceOf(Bundle::class, $plugin);
+    }
+
+    public function testAddConfiguration()
+    {
+        $arrayNode = new ArrayNodeDefinition('node');
+
+        $plugin = new GuzzleBundleWssePlugin();
+        $plugin->addConfiguration($arrayNode);
+
+        $node = $arrayNode->getNode();
+
+        $this->assertFalse($node->isRequired());
+        $this->assertTrue($node->hasDefaultValue());
+        $this->assertSame(
+            ['username' => null, 'password' => null, 'created_at' => null],
+            $node->getDefaultValue()
+        );
+    }
+
+    public function testGetPluginName()
+    {
+        $this->assertEquals('wsse', (new GuzzleBundleWssePlugin())->getPluginName());
     }
 }
